15bcb80179cb3e7ac50ea34fc23643a306745518
Manish Kumar <manishkk@users.noreply.github.com>
2016-12-28 15:39:46 +0100

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -1,33 +1,35 @@
 from django.shortcuts import render
-
 # Create your views here.
 from django.shortcuts import render
 import dpkt
 import scapy
 import socket
 import struct
-
 from django.http import HttpResponse
 from pcapfile import savefile
 from pcapfile.protocols.linklayer import ethernet
 from pcapfile.protocols.network import ip
 import binascii
 
-
 testcap = open("/home/manish/PycharmProjects/pcap/webapp/Test.pcap")
 capfile = savefile.load_savefile(testcap, verbose=True)
 
+ip_packet =ip.IP(binascii.unhexlify(ethernet.Ethernet(capfile.packets[0].raw()).payload));
+mac_packet = ethernet.Ethernet(capfile.packets[0].raw());
+packet = capfile.packets[0];
+
+
 
 def index(request):
-    answer = [
+        answer = [
+
+                 str(packet.timestamp),
+                 str(mac_packet.src),
+                 str(mac_packet.dst),
+                 str(ip_packet.src),
+                 str(ip_packet.dst),
+                 str(packet.packet_len),
 
 
-        str(ip.IP(binascii.unhexlify(ethernet.Ethernet(capfile.packets[0].raw()).payload))),
-        str(capfile.packets[0].timestamp),
-        str(ethernet.Ethernet(capfile.packets[0].raw()))
-
-    ]
-
-    return HttpResponse("<br>".join(answer))
-
-
+                ]
+        return HttpResponse("<br>".join(answer))
