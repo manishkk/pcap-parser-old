94b9c3ce230c13c00897c9f2dd3e9d4e9d1956db
Manish Kumar <manishkk@users.noreply.github.com>
2017-01-02 13:08:25 +0100

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -9,21 +9,29 @@ from django.http import HttpResponse
 from pcapfile import savefile
 from pcapfile.protocols.linklayer import ethernet
 from pcapfile.protocols.network import ip
+from pcapfile.protocols.transport import udp
+
 import binascii
 
-testcap = open("/home/manish/PycharmProjects/pcap/webapp/Test_2.pcap")
+testcap = open("/home/manish/PycharmProjects/pcap/webapp/Test_1.pcap")
 capfile = savefile.load_savefile(testcap, verbose=True)
-
-
-
 
 
 def index(request):
                 answer = []
                 for packet in capfile.packets:
                     try:
-                        ip_packet = ip.IP(binascii.unhexlify(ethernet.Ethernet(packet.raw()).payload))
+                        udp_packet = None
+                        ptype = " "
                         mac_packet = ethernet.Ethernet(packet.raw());
+                        if mac_packet.type == 2048:
+                            ip_packet = ip.IP(binascii.unhexlify(ethernet.Ethernet(packet.raw()).payload))
+                            ptype = "IPv4"
+                            if ip_packet.p == 17:
+                                udp_packet = udp.UDP(binascii.unhexlify(ethernet.Ethernet(packet.raw()).payload))
+                                ptype = "UDP"
+                        else:
+                                ptype = "unknown"
                         answer.extend([
 
                              str(packet.timestamp),   #timestamp
@@ -31,14 +39,28 @@ def index(request):
                              str(mac_packet.dst),   #Receiver Mac Address
                              str(ip_packet.src),    #Sender IP
                              str(ip_packet.dst),    #Receiver IP
-                             str(ip_packet.len),    #Payload lenght with IP header
-                             str(ip_packet),        #Print whole packet information
+                             str(ip_packet.len)+ " " +"byte",    #Payload lenght with IP header
+                           #  str(ip_packet),        #Print whole packet information
+
                              str(mac_packet.type),  #Protocol Type
-                             str(len(capfile.packets)), #Lenght of Pcacp file
 
-                            ""
 
+                             str(len(capfile.packets)) + " " + "Packets",  # Lenght of Pcacp file
+                             str(ptype),
                             ])
+
+                        if udp_packet is not None:
+                            answer.extend([
+                                str(udp_packet.dst_port),
+                                str(udp_packet.src_port)
+                            ])
+                        else:
+                            answer.extend([
+                                str(ptype)   #add port details of TCP packets
+                            ])
+
+                        answer.append("")
+
                     except:
                         import traceback
                         traceback.print_exc()
